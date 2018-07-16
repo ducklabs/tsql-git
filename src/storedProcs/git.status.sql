3e53a99c9c87e5593f2a5b2f1c21b32064d31d76
Joey <j.lai@live.ca>
2018-07-16 13:25:20 -0600

    added git status

---
+++ src/storedProcs/git.status.sql
@@ -0,0 +1,25 @@
+USE [tsqlGit]
+GO
+/****** Object:  StoredProcedure [git].[status]    Script Date: 7/16/2018 1:25:00 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		Joey Lai
+-- Create date: July 16, 2018
+-- Description:	Get git status
+-- =============================================
+ALTER PROCEDURE [git].[status]
+AS
+BEGIN
+
+	declare @gitDirectory varchar(255)
+	select @gitDirectory = repoDirectory from git.config
+
+	declare @gitStatusShell varchar(4000) = concat('cd ', @gitDirectory, ' & git status')
+	print 'running: ' + @gitStatusShell
+	exec xp_cmdshell @gitStatusShell
+
+END
