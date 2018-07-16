1fcc9306ab16d2f5cc0293728984aa305b334c87
Joey <j.lai@live.ca>
2018-07-16 16:15:36 -0600

    git pull command

---
+++ src/storedProcs/git.pullLatest.sql
@@ -0,0 +1,26 @@
+USE [tsqlGit]
+GO
+/****** Object:  StoredProcedure [git].[pushToRepo]    Script Date: 7/16/2018 4:13:25 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Joey Lai
+-- Create date: July 16, 2018
+-- Description:	Pulls latest changes to remote repo
+-- =============================================
+ALTER PROCEDURE [git].[pullLatest]
+AS
+BEGIN
+
+	declare @gitDirectory varchar(255)
+	select @gitDirectory = repoDirectory from git.config
+
+	declare @gitPullShell varchar(4000) = concat('cd ', @gitDirectory, ' & git pull')
+	print 'running: ' + @gitPullShell
+	exec xp_cmdshell @gitPullShell
+
+END
+
+-- exec git.pullLatest
